1e097c9202492c7a4d801620b27c01ae696a75ff
Corentin-B <banteignie.c@gmail.com>
2020-07-06 16:21:53 +0200

    Front Done, ready for back

--- src/components/CompanyDetails.vue
+++ src/components/CompanyDetails.vue
@@ -1,8 +1,26 @@
 <template>
-  <v-expansion-panels>
+  <v-expansion-panels class="elementPanel">
     <v-expansion-panel>
-      <v-expansion-panel-header></v-expansion-panel-header>
-      <v-expansion-panel-content>azeaz</v-expansion-panel-content>
+      <v-expansion-panel-header>
+        <h4>{{company.name}}</h4>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        <div class="buttonContainer">
+          <v-btn color="error" class="bottomButton" @click="dialog = true">Supprimer</v-btn>
+          <v-dialog v-model="dialog" max-width="300">
+            <v-card>
+              <v-card-title class="headline">Voulez vous supprimer cette société ?</v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn color="error" @click="dialog = false">Oui</v-btn>
+
+                <v-btn color="primary" @click="deleteCompany">Non</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
+      </v-expansion-panel-content>
     </v-expansion-panel>
   </v-expansion-panels>
 </template>
@@ -10,12 +28,26 @@
 <script>
 export default {
   name: "CompanyDetails",
-  props: {
-  },
-  methods: {}
+  data: () => ({
+    dialog: false
+  }),
+  props: { company: Object },
+  methods: {
+    deleteCompany() {
+      //TODO Call API
+      this.dialog = false
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.buttonContainer {
+  text-align: right;
+}
+
+.bottomButton {
+  margin-right: 1%;
+}
 </style>
--- src/components/ListElements.vue
+++ src/components/ListElements.vue
@@ -1,47 +1,53 @@
 <template>
   <div>
     <v-card class="searchCard">
-       <v-row justify="center" align="center">
-        <v-select :items="dropdown_categories" filled label="Catégories" dense v-model="categories"></v-select>
-        <v-text-field v-model="searchfilter" label="Recherche" clearable dense></v-text-field>
-        <v-select :items="dropdown_numberelement" filled label="Nombre d'éléments" dense></v-select>
+      <v-row justify="center" align="center" class="rowTopElements">
+        <v-select
+          :items="dropdown_category"
+          filled
+          label="Catégories"
+          @change="changecategorie($event)"
+          v-model="category"
+          class="topcardElements"
+        ></v-select>
+        <v-text-field v-model="searchfilter" label="Recherche" clearable class="searchBar"></v-text-field>
+        <v-select
+          :items="dropdown_numberelement"
+          filled
+          label="Nombre d'éléments"
+          v-model="numberelement"
+          class="topcardElements"
+        ></v-select>
       </v-row>
-      <v-row justify="center" align="center" v-if="categories = 'Ordinateur'">
-          <v-chip draggable>Nom</v-chip>
-          <v-chip draggable>Date de mise en service</v-chip>
-          <v-chip draggable>Date de fin de serivce</v-chip>
-          <v-chip draggable>Société</v-chip>
-        </v-row>
-        <v-row justify="center" align="center" v-if="categories = 'Société'">
-          <v-chip draggable>Nom</v-chip>
-        </v-row>
-      <v-btn color="primary" @click="search">Rechercher</v-btn>
+      <v-row justify="center" align="center" v-if="category === 'Ordinateur'">
+        <v-chip-group active-class="primary--text" v-model="sortelement">
+          <v-chip>Nom</v-chip>
+          <v-chip>Date de mise en service</v-chip>
+          <v-chip>Date de fin de serivce</v-chip>
+          <v-chip>Société</v-chip>
+        </v-chip-group>
+      </v-row>
+      <v-row justify="center" align="center" v-if="category === 'Société'">
+        <v-chip-group active-class="primary--text" v-model="sortelement">
+          <v-chip>Nom</v-chip>
+        </v-chip-group>
+      </v-row>
+      <div class>
+        <v-btn color="primary" @click="search" class="searchButton">Rechercher</v-btn>
+      </div>
     </v-card>
+
     <div v-for="(element, i) in lister" :key="i" class="elementsPanel">
       <div v-for="(elem, o) in element" :key="o">
-        <v-expansion-panels class="elementPanel">
-          <v-expansion-panel>
-            <v-expansion-panel-header>
-              <h4>{{elem.name}}</h4>
-            </v-expansion-panel-header>
-            <v-expansion-panel-content>
-              <ul v-if="elem.introduced">
-                <li>Introduced : {{elem.introduced}}</li>
-                <li>Discontinued : {{elem.discontinued}}</li>
-                <li>Company : {{elem.company.name}}</li>
-              </ul>
-              <v-btn color="primary" @click="sheet = !sheet">Edit</v-btn>
-              <v-bottom-sheet v-model="sheet">
-                <v-sheet class="text-center" height="200px">
-                  <v-btn color="red" @click="sheet = !sheet">Close</v-btn>
-                  <EditElement style="editelement" />
-                  <!--TODO Commande pour installation du front-->
-                </v-sheet>
-              </v-bottom-sheet>
-              <v-btn color="error">Delete</v-btn>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
+        <ComputerDetails
+          v-if="responceStatus === '200' && responceUrl === '\\computer'"
+          v-bind:computer="elem"
+        />
+        <CompanyDetails
+          v-else-if="responceStatus === '200' && responceUrl === '\\company'"
+          v-bind:company="elem"
+        />
+        <div v-else>Error</div>
       </div>
     </div>
   </div>
@@ -49,23 +55,31 @@
 
 <script>
 import * as data from "../MOCK/mockComputer";
-import EditElement from "./EditElement";
+import ComputerDetails from "./ComputerDetails";
+import CompanyDetails from "./CompanyDetails";
 
 export default {
   name: "ListElements",
   data: () => ({
-    sheet: false,
     elements: Object,
-    dropdown_categories: ["Ordinateur" ,"Société"],
-    dropdown_numberelement: ["10", "50", "100"],
+    category: "",
     searchfilter: "",
-    categories: "",
     numberelement: "",
+    sortelement: "",
+    responceStatus: "200",
+    responceUrl: "\\computer",
+    dropdown_category: ["Ordinateur", "Société"],
+    dropdown_numberelement: ["10", "50", "100"]
   }),
-  components: { EditElement },
+  components: { ComputerDetails, CompanyDetails },
   props: {},
   methods: {
     search() {
+      //TODO Call API
+    },
+    changecategorie(event) {
+      this.category = event;
+      this.sortelement = "";
     }
   },
   computed: {
@@ -80,6 +94,12 @@ export default {
 <style scoped>
 .searchCard {
   margin: 4vh 15vw;
+  text-align: center;
+}
+
+.rowTopElements {
+  margin: 0 3px;
+  padding-top: 3px;
 }
 
 .elementsPanel {
@@ -92,5 +112,17 @@ export default {
 
 .editelement {
   margin-bottom: 3vw;
+}
+
+.topcardElements {
+  width: 18.6vw;
+}
+
+.searchBar {
+  margin: 1vh;
+}
+
+.searchButton {
+  top: 2.5vh;
 }
 </style>
