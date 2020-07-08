05f27a402c405b30269168d02936a04f8033cdf2
cyril <claincyril0@gmail.com>
2020-07-08 09:51:49 +0200

    ajout company modal

--- src/api/company_api.js
+++ src/api/company_api.js
@@ -8,6 +8,10 @@ export default class CompanyApi {
     findAll() {
         return this.axios.get('/companies',{ headers: {"authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJldGllbm5lIiwiZXhwIjoxNTk0MTQ0MzYyLCJpYXQiOjE1OTQxMDgzNjJ9.cLukJIOupcFC6vmiVldBRZVjdw8vnziaxaae82OCvUEgbKulFzwlAkSiCFmaCIqDl62LQVgASvdX6yWC726IEw"}});
     }
+
+    create(company) {
+        return this.axios.post('/companies', company, { headers: {"Content-Type": "application/json", "authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJldGllbm5lIiwiZXhwIjoxNTk0MTc3MDIxLCJpYXQiOjE1OTQxNDEwMjF9.ry-bV3lRFNDiT890awuCTuWhODedkQ2mtKDbAim4uPuOH-D-g8A2j1eYK5Qda09tOklvLy-b2OlIDhXiIw1UiQ"}})
+    }
 }
 
 export const companyApi = new CompanyApi(axios)
