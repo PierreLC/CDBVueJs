cf9d3aee0dce032792bffd3d7793acf3d914c387
Corentin-B <banteignie.c@gmail.com>
2020-07-09 19:50:23 +0200

    WIP front rebase

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,67 +1,68 @@
 <template>
-  <div>
-    <v-card color="grey lighten-4" flat tile>
-      <v-toolbar prominent extended>
-        <router-link to="/Dashboard">
-          <v-btn class="header">
-            <v-icon>mdi-home</v-icon>
-          </v-btn>
-        </router-link>
+  <div class="mainElement">
+    <v-row justify="center" align="center" class="rowHeader">
+      <router-link to="/Dashboard">
+        <v-btn class="headerButton">
+          <v-img src=""></v-img>
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+      </router-link>
 
-        <v-spacer></v-spacer>
+      <v-btn outlined color="indigo" dark @click="dialog = !dialog" class="headerButton">
+        <v-icon>mdi-plus</v-icon>
+        {{ $t("HEADER.ADD") }}
+      </v-btn>
+      <v-dialog v-model="dialog" persistent>
+        <v-card>
+          <AddElement @clickCloseAdd="emitCloseAdd" />
+        </v-card>
+      </v-dialog>
+      <v-btn
+        outlined
+        color="indigo"
+        dark
+        @click="CompanyModal = !CompanyModal"
+        class="headerButton"
+      >
+        <v-icon>mdi-plus</v-icon>Add Company
+      </v-btn>
+      <v-dialog v-model="CompanyModal" persistent>
+        <v-card>
+          <AddCompany @clickeModal="emitBoolClose" />
+        </v-card>
+      </v-dialog>
 
-        <v-row justify="center">
-          <v-btn outlined color="indigo" dark @click="dialog = !dialog">
-            <v-icon>mdi-plus</v-icon>{{ $t("HEADER.ADD") }}
-          </v-btn>
-          <v-dialog v-model="dialog" persistent>
-            <v-card>
-              <AddElement @clickCloseAdd="emitCloseAdd" />
-            </v-card>
-          </v-dialog>
-          <v-btn
-            outlined
-            color="indigo"
-            dark
-            @click="CompanyModal = !CompanyModal"
-          >
-            <v-icon>mdi-plus</v-icon>Add Company
-          </v-btn>
-          <v-dialog v-model="CompanyModal" persistent>
-            <v-card>
-              <AddCompany @clickeModal="emitBoolClose" />
-            </v-card>
-          </v-dialog>
-        </v-row>
+      <div class="languagesDiv">
+        <v-menu top offset-y>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="headerButton" fab small dark>
+              <v-icon v-bind="attrs" v-on="on">mdi-earth</v-icon>
+            </v-btn>
+          </template>
 
-        <div class="languagesDiv">
-          <v-menu top offset-y>
-            <template v-slot:activator="{ on, attrs }">
-              <v-icon class="languagesButton" v-bind="attrs" v-on="on"
-                >mdi-earth</v-icon
-              >
-            </template>
+          <v-list>
+            <v-list-item
+              v-for="(lang, i) in langs"
+              :key="`Lang${i}`"
+              :value="lang"
+              @click="changeLang(lang)"
+            >
+              <v-list-item-title>{{ lang }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
 
-            <v-list>
-              <v-list-item
-                v-for="(lang, i) in langs"
-                :key="`Lang${i}`"
-                :value="lang"
-                @click="changeLang(lang)"
-              >
-                <v-list-item-title>{{ lang }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </div>
+      <v-btn color="headerButton" fab small dark>
+        <v-icon>mdi-bell</v-icon>
+      </v-btn>
+      <router-link to="/Authentication">
+        <v-btn color="headerButton" fab small dark>
+          <v-icon>mdi-account</v-icon>
+        </v-btn>
+      </router-link>
+    </v-row>
 
-        <v-icon class="notificationsButton">mdi-bell</v-icon>
-
-        <router-link to="/Authentication">
-          <v-icon class="logoutButton">mdi-account</v-icon>
-        </router-link>
-      </v-toolbar>
-    </v-card>
     <div class="LBC"></div>
   </div>
 </template>
@@ -76,7 +77,7 @@ export default {
   data: () => ({
     CompanyModal: false,
     dialog: false,
-    langs: ["fr", "en"],
+    langs: ["fr", "en"]
   }),
 
   props: {},
@@ -90,19 +91,27 @@ export default {
     },
     emitCloseAdd(value) {
       this.dialog = value;
-    },
-  },
+    }
+  }
 };
 </script>
 
 <style scoped>
-.dashboard {
-  position: absolute;
-  text-align: center;
+.mainElement {
+  background-color: #3b5998;
+}
+
+.rowHeader {
+  padding: 1%;
+}
+
+.headerButton {
+  margin: 1%;
+  background-color: white;
 }
 
 .LBC {
-  position: relative;
+  position: absolute;
   background-color: #3b5998;
   left: 0;
   right: 0;
@@ -110,9 +119,5 @@ export default {
   height: 150px;
   width: auto;
   border-radius: 0 0 50% 50%/0 0 100% 100%;
-}
-
-.addButton {
-  text-decoration-color: white;
 }
 </style>
